657229e30baec138d0fd6726227be46dbd4b4fed
SamuelGitau-School <samuel.gitau4@student.moringaschool>
2026-09-10 10:58:51 +0300

    Remove all repository content

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-models package
-Contains all domain classes for TaskFlow: Person, User, Project, Task.
-"""
--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-utils package
-Helper modules: JSON storage, decorators (auth/logging), and validators.
-"""
